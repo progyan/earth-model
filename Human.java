7ff8d3ccbd9224bce3ad0742bdde0cb41550ebe0
Yan <yanb@protonmail.com>
2018-10-14 15:52:52 +0300

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,7 +1,21 @@
 public class Human {
+    public boolean died;
     private String name;
-    
+    private int thingsCanDo;
+
+    public Human(String name){
+        this.name = name;
+        this.thingsCanDo = Integer.parseInt(Long.toString(Math.round((Math.random() * 10) + 10)));
+    }
+
     public String getName(){
-        return this.name;
+        if(!this.died)
+            return this.name;
+        else
+            return null;
+    }
+
+    public void die(){
+         this.died = true;
     }
 }
