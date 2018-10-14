795be507ede5ad75031ff5963992da6703c30539
Yan <yanb@protonmail.com>
2018-10-14 15:24:38 +0300

    Added fields to Continent.java

--- Continent.java
+++ Continent.java
@@ -1,3 +1,15 @@
-public class Continent {
+import java.util.ArrayList;
 
+public class Continent {
+    private String name;
+    private long population;
+    private ArrayList humans = new ArrayList();
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
 }
