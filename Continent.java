216de79d31ea24154962d7f7ffea05019aa03bf4
Yan <yanb@protonmail.com>
2018-10-15 20:28:52 +0300

    Added iterate() method to Continent.java

--- Continent.java
+++ Continent.java
@@ -3,16 +3,23 @@ import java.util.ArrayList;
 public class Continent {
     private String name;
     private long population;
-    private ArrayList humans = new ArrayList();
-    
-    public Continent(String name, ArrayList<Human> humans){
+    private ArrayList<Human> humans;
+
+    public Continent(String name, ArrayList<Human> humans) throws InterruptedException{
         this.name = name;
         this.population = humans.size();
         this.humans = humans;
+        iterate();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void iterateNTimes(int n) throws InterruptedException {
+        for(int i = 0; i < n; i++){
+            iterate();
+        }
     }
 
     public long getPopulation() {
@@ -21,5 +28,15 @@ public class Continent {
 
     public ArrayList getHumans() {
         return humans;
+    }
+
+    private void iterate() throws InterruptedException{
+        ArrayList<Human> humansToAdd = new ArrayList<Human>();
+        for(Human human : humans){
+            if(Math.random() > 0.25)
+                humansToAdd.add(human.mother());
+        }
+        humans.addAll(humansToAdd);
+        Thread.sleep(100);
     }
 }
